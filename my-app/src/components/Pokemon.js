a9e9f0ca16db0bb9ac9b105296997ecade444fe2
Serge Chvorun <elifnull@protonmail.com>
2022-03-11 01:08:44 -0500

    pokemon api finished

--- my-app/src/components/Pokemon.js
+++ my-app/src/components/Pokemon.js
@@ -3,7 +3,23 @@ import React, { useEffect, useState } from 'react';
 const Pokemon = () => {
     const [pokedex, setPokedex] = useState([]);
 
-
+    useEffect(()=>{
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=1126")
+        .then(res =>{ console.log(res)
+            return res.json()})
+        .then(res => setPokedex(res.results))
+        .catch(err => console.loge(err))
+    },[]);
+    
+    return(
+        <div>
+            Pokemon!
+            <ul>
+                {pokedex.map((value,index) => {
+                    return(<li className='ulist'>{value.name.toUpperCase()}</li>)})}
+            </ul>   
+        </div>
+    )
 
 }
 
