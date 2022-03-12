57ee7cc933ff1037b983184e7b26d3100b4be5e0
Serge Chvorun <elifnull@protonmail.com>
2022-03-11 22:54:05 -0500

    axios final update

---
+++ my-app/src/components/Pokemon_axios.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const PokemonAxios = () => {
+    const [pokedex, setPokedex] = useState([]);
+
+    useEffect(()=>{
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=1126")
+        .then(res => {
+            console.log(res);
+            return setPokedex(res.data.results);})
+        .catch(err => console.log(err))
+    },[]);
+    
+    return(
+        <div>
+            <ul>
+                <h2>Pokemon! (Axios)</h2>
+                {pokedex.map((value,index) => {
+                    return(<li className='ulist'>{value.name.toUpperCase()}</li>)})}
+            </ul>   
+        </div>
+    )
+
+}
+
+export default PokemonAxios;
