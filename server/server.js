d50524ea6d3ae9fbcaf370a84da2b52ad1258292
Serge Chvorun <elifnull@protonmail.com>
2022-03-14 16:39:56 -0400

    server tests

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 
 const app = express();
+const port = 8000;
 
+console.log("ilove nodemon")
+app.get("/api", (req, res) => {
+    console.log(req );
+    res.json({ message: "Hello World" });
+});
+app.post("/api/welcome/nud", (req, res) => {
+    res.json({message: ["MERN Goodness", "MERN baby MERN"], Broly: req.body})
+});
 
-
-app.listen(8000, ()=>console.log("you have succesfully connected to port 8k"))
+app.listen( port, () => console.log(`Listening on port: ${port}`) );
