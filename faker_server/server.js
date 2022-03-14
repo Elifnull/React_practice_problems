046a44d876082dd08e9447c0eccf2c7c0d1bb03c
Serge Chvorun <elifnull@protonmail.com>
2022-03-14 19:47:15 -0400

    faker server test success

--- faker_server/server.js
+++ faker_server/server.js
@@ -3,12 +3,46 @@ const faker = require("faker");
 
 const app = express();
 const port = 8000;
+const createUser = () => {
+    const user = {
+        id: faker.datatype.uuid(),
+        name: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        phoneNumber: faker.phone.phoneNumber(),
+        email: faker.internet.email(),
+        password: faker.internet.password()
+    }
+    return user;
+};
 
-
+const createCompany = () => {
+    const company = {
+        id: faker.datatype.uuid(),
+        name: faker.company.companyName(),
+        address:{
+            street:faker.address.streetAddress(),
+            city:faker.address.cityName(),
+            state:faker.address.state(),
+            zipcode:faker.address.zipCode(),
+            country:faker.address.country(),
+        },
+    }
+    return company;
+};
 //some important app middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.get("/api/users/new", (req, res) =>{
+    res.json(createUser())
+});
 
+app.get("/api/companies/new", (req, res)=>{
+    res.json(createCompany())
+});
+
+app.get("/api/user/company",(req,res)=>{
+    res.json([createCompany(),createUser()])
+});
 
 app.listen(port, () => console.log(`listening port: ${port}`));
