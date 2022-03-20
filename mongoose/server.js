ae84269f5862217285b7046948e131a00b999b85
Serge Chvorun <elifnull@protonmail.com>
2022-03-20 19:27:14 -0400

    mongoose schema added

--- mongoose/server.js
+++ mongoose/server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const databaseName = "admin";
+
 const app = express();
 
 
@@ -6,9 +9,33 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+mongoose.connect("mongodb://localhost/" + databaseName, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log(`You connected to the ${databaseName} database`);
+})
+.catch((err)=>{
+    console.log(`there was an error connecting to the ${databaseName} database`);
+})
+
+// create the model - shape of the documents inside of a collection
+const MongooseSchema = new mongoose.Schema({
+    name: { 
+        type: String,
+        minlength: 3,
+    },
+    age: {
+        type: Number,
+    },
+    isFriendly: {
+        type: Boolean,
+    },
+}, { timestamps: true });
 //routes
-app.get();
+
 
 app.listen(8000, () => {
     console.log("You have successfully started your server on port 8k");
-})
+});
