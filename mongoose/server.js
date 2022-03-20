0ea2ef719f97f1835fe150f54fbdedd6714302ca
Serge Chvorun <elifnull@protonmail.com>
2022-03-20 17:48:50 -0400

    mongoose and server update

--- mongoose/server.js
+++ mongoose/server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+
+// this is needed to allow for JASON post and get requests
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+//routes
+app.get();
+
+app.listen(8000, () => {
+    console.log("You have successfully started your server on port 8k");
+})
